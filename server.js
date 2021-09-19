11daab0997360eb4657d6c29574d5dd4ef985922
M-Minkov <MinkoMinkov@outlook.co.nz>
2021-09-20 08:31:03 +1200

    Added application to date
    Has basic routes, socketIO, database, and passport configuration.

---
+++ server.js
@@ -0,0 +1,102 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const session = require('express-session');
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.set('view engine', 'pug');
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
+  let currentUsers = 0;
+  
+  io.on('connection', socket => {
+  ++currentUsers;
+  io.emit('user', {
+  name: socket.request.user.name,
+  currentUsers,
+  connected: true
+});
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { name: socket.request.user.name, message });
+    });
+  console.log('user ' + socket.request.user.username + ' connected');
+  socket.on('disconnect', () => {
+        console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
+});
+});
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+  
+}).catch((e) => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+});
+
+
+
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
+});
